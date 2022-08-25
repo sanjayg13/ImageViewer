c152b0ee26f9cb3788fa3a841df9559484fdcc92
sanjayg13 <52193097+sanjayg13@users.noreply.github.com>
2022-08-25 14:23:42 +0530

    Add files via upload

---
+++ ImageViewer.Header.Test/HeaderModuleTests.cs
@@ -0,0 +1,139 @@
+﻿using ImageViewer.HeaderView;
+using ImageViewer.Test.Utilities;
+using ImageViewer.Ui.Utilities.Events;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using Prism.Events;
+using Prism.Regions;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ImageViewer.Header.Test
+{
+    [Apartment(ApartmentState.STA)]
+    public class TestHeaderModule
+    {
+        private IUnityContainer _container;
+        private IEventAggregator _eventAggregator;
+        private HeaderViewModule _headerViewModule;
+
+        /// <summary>
+        ///     SetUp
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            TestHelper.SetupTestApplication();
+            _container = new UnityContainer();
+
+            var bootstrapper = new TestBootStrapper();
+            bootstrapper.Run();
+
+            SetupMocks();
+            _headerViewModule = _container.Resolve<HeaderViewModule>();
+            _headerViewModule.Initialize();
+        }
+
+        /// <summary>
+        ///     TearDown
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestHelper.TestApplication.MainWindow != null)
+            {
+                TestHelper.TestApplication.MainWindow.Content = null;
+            }
+
+            _headerViewModule = null;
+            _container.Dispose();
+            _container = null;
+        }
+
+        /// <summary>
+        ///     Test to check the header content.
+        /// </summary>
+        [Test]
+        public void TestHeaderContent()
+        {
+             HeaderView.Views.Header header = _container.Resolve<HeaderView.Views.Header>();
+            TestHelper.InitializeTestWindow(header);
+
+            var searchButton = new List<Button>();
+            TestHelper.FindVisualChild(header, "ImgSearchButton", searchButton);
+
+            var undoButton = new List<Button>();
+            TestHelper.FindVisualChild(header, "UndoButton", undoButton);
+
+            var searchTextbox = new List<TextBox>();
+            TestHelper.FindVisualChild(header, "ImgSearchTag", searchTextbox);
+
+            Assert.AreEqual(Visibility.Visible,searchButton[0].Visibility);
+            Assert.AreEqual(Visibility.Visible, undoButton[0].Visibility);
+            Assert.AreEqual(Visibility.Visible, searchTextbox[0].Visibility);
+        }
+
+        /// <summary>
+        ///     Test to check the header undo button click
+        /// </summary>
+        [Test]
+        public void TestHeaderUndoButton()
+        {
+            HeaderView.Views.Header header = _container.Resolve<HeaderView.Views.Header>();
+            TestHelper.InitializeTestWindow(header);
+
+            var undoButton = new List<Button>();
+            TestHelper.FindVisualChild(header, "UndoButton", undoButton);
+
+            var searchButton = new List<Button>();
+            TestHelper.FindVisualChild(header, "ImgSearchButton", searchButton);
+
+
+            Assert.AreEqual(undoButton[0].IsEnabled,false);
+
+            searchButton[0].Command.Execute(null);
+            searchButton[0].Command.Execute(null);
+
+            Assert.AreEqual(undoButton[0].IsEnabled, true);
+
+            undoButton[0].Command.Execute(null);
+
+
+            Assert.AreEqual(undoButton[0].IsEnabled, false);
+        }
+
+        /// <summary>
+        ///     Test to check the header Search button click
+        /// </summary>
+        [Test]
+        public void TestSearchButton()
+        {
+            HeaderView.Views.Header header = _container.Resolve<HeaderView.Views.Header>();
+            TestHelper.InitializeTestWindow(header);
+
+            var searchButton = new List<Button>();
+            TestHelper.FindVisualChild(header, "ImgSearchButton", searchButton);
+
+            bool isSearchInvoked = false;
+            _eventAggregator.GetEvent<InvokeImageSearchEvent>().Subscribe((string s) => isSearchInvoked = true);
+
+            searchButton[0].Command.Execute(null);
+
+
+            Assert.AreEqual(isSearchInvoked, true);
+
+        }
+
+        private void SetupMocks()
+        {
+            _container.RegisterType<IRegionManager, RegionManager>();
+            _container.RegisterType<IEventAggregator, EventAggregator>();
+            var regionManager = _container.Resolve<IRegionManager>();
+            _container.RegisterInstance(regionManager, new ContainerControlledLifetimeManager());
+            _eventAggregator = _container.Resolve<IEventAggregator>();
+            _container.RegisterInstance(_eventAggregator, new ContainerControlledLifetimeManager());
+        }
+    }
+}
---
+++ ImageViewer.ImageGridView.Test/ImageGridModuleTests.cs
@@ -0,0 +1,117 @@
+﻿using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using ImageViewer.ImageGridView.Models;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using ImageViewer.Test.Utilities;
+using Prism.Regions;
+using Rhino.Mocks;
+using ImageViewer.Ui.Utilities.Interfaces;
+using FlickerComponent.Interfaces;
+using ImageViewer.ImageGridView.Views;
+using ImageViewer.Ui.Utilities.Events;
+
+namespace ImageViewer.ImageGridView.Test
+{
+    [Apartment(ApartmentState.STA)]
+    public class ImageGridModuleTests
+    {
+        private IUnityContainer _container;
+        private IEventAggregator _eventAggregator;
+        private ImageGridViewModule _imageGridViewModule;
+
+        /// <summary>
+        ///     SetUp
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            TestHelper.SetupTestApplication();
+            _container = new UnityContainer();
+
+            var bootstrapper = new TestBootStrapper();
+            bootstrapper.Run();
+
+            SetupMocks();
+            _imageGridViewModule = _container.Resolve<ImageGridViewModule>();
+            _imageGridViewModule.Initialize();
+        }
+
+        [Test]
+        public void TestImageGridModule()
+        {
+            var imageGrid = _container.Resolve<ImageGrid>();
+            TestHelper.InitializeTestWindow(imageGrid);
+
+            var imageGridContentControl = new List<ContentControl>(); 
+            TestHelper.FindVisualChild(imageGrid, "ImageGridViewContentControl", imageGridContentControl);
+
+            var extendedImageControl = new List<ContentControl>();
+            TestHelper.FindVisualChild(imageGrid, "ExtendedImageViewContentControl", extendedImageControl);
+
+            _eventAggregator.GetEvent<InvokeImageSearchEvent>().Publish(null);
+
+            var listBoxControl = new List<ItemsControl>();
+            TestHelper.FindVisualChild(imageGrid, "ImageListBox", listBoxControl);
+
+            listBoxControl[0]?.GetBindingExpression(ItemsControl.ItemsSourceProperty)?.UpdateTarget();
+
+            var listBoxItemGrid = new List<Grid>();
+            TestHelper.FindVisualChild(imageGrid, "ImageView", listBoxItemGrid);
+            TestHelper.TestApplication?.Dispatcher?.Invoke(() => { }, DispatcherPriority.Render);
+
+            var selectedItemIndex = 0;
+            RaiseMouseUpEvent(listBoxItemGrid[selectedItemIndex],UIElement.PreviewMouseDownEvent);
+
+
+            Assert.AreEqual(imageGridContentControl[0].Visibility, Visibility.Visible);
+            Assert.AreEqual(imageGridContentControl[0].Visibility, Visibility.Collapsed);
+        }
+
+        private void SetupMocks()
+        {
+            _container.RegisterType<IRegionManager, RegionManager>();
+            _container.RegisterType<IEventAggregator, EventAggregator>();
+             var regionManager = _container.Resolve<IRegionManager>();
+            _container.RegisterInstance(regionManager, new ContainerControlledLifetimeManager());
+            _eventAggregator = _container.Resolve<IEventAggregator>();
+            _container.RegisterInstance(_eventAggregator, new ContainerControlledLifetimeManager());
+
+            var imageListModelProvider = MockRepository.GenerateMock<IModelProvider<ImageListModel>>();
+
+            var imageListModel = new ImageListModel();
+            imageListModel.Images.Add(new ImageModel(){IsSelected = true});
+            imageListModel.Images.Add(new ImageModel());
+            
+            imageListModelProvider
+                .Stub(x => x.GetModel(Arg<string>.Is.Anything))
+                .WhenCalled(_ =>
+                {
+                    imageListModelProvider.Stub(x => x.GetModel(Arg<string>.Is.Anything))
+                        .Return( imageListModel);
+                });
+
+            _container.RegisterInstance(imageListModelProvider, new ContainerControlledLifetimeManager());
+        }
+
+        private static FrameworkElement RaiseMouseUpEvent(FrameworkElement frameworkElement, RoutedEvent routedEvent)
+        {
+            var eventArgs = new MouseButtonEventArgs(Mouse.PrimaryDevice, 0, MouseButton.Left)
+            {
+                RoutedEvent = routedEvent,
+                Source = frameworkElement
+            };
+            frameworkElement.RaiseEvent(eventArgs);
+            return frameworkElement;
+        }
+    }
+}
