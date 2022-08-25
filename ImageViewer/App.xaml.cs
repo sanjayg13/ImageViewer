238215f15b2a3dd7af67fc998db47db635577d46
sanjayg13 <52193097+sanjayg13@users.noreply.github.com>
2022-08-25 14:21:29 +0530

    Add files via upload

---
+++ ImageViewer/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ImageViewerBootStrapper _bootStrapper;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _bootStrapper = new ImageViewerBootStrapper();
+            _bootStrapper.Run();
+        }
+    }
+}
---
+++ ImageViewer/ImageViewerBootStrapper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ImageViewer.HeaderView;
+using ImageViewer.ImageGridView;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace ImageViewer
+{
+    /// <summary>
+    ///  Boot strapper class to configure container and initialize components.
+    /// </summary>
+    public class ImageViewerBootStrapper : UnityBootstrapper
+    {
+        #region Overridden Methods      
+        public override void Run(bool runWithDefaultConfiguration)
+        {
+            base.Run(runWithDefaultConfiguration);
+        }
+            
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+
+
+        /// <summary>
+        ///     CreateModule Catalog
+        /// </summary>
+        /// <returns></returns>
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            var moduleCatalog = new ConfigurationModuleCatalog();
+            return moduleCatalog;
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            moduleCatalog.AddModule(typeof(HeaderViewModule));
+            moduleCatalog.AddModule(typeof(ImageGridViewModule));
+
+        }
+        #endregion
+    }
+}
